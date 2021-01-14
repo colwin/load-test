23c56c0fdb3bd895552a83558deb8ed71a8fa8ba
Colwin Fernandes <colwin@opcito.com>
2021-01-14 13:05:54 +0530

    Fixed base64encode

--- locustfile.py
+++ locustfile.py
@@ -8,7 +8,7 @@ class WebTasks(TaskSet):
 
     @task
     def load(self):
-        base64string = base64.encodestring('%s:%s' % ('user', 'password')).replace('\n', '')
+        base64string = base64.b64encode('%s:%s' % ('user', 'password')).replace('\n', '')
 
         catalogue = self.client.get("/catalogue").json()
         category_item = choice(catalogue)
